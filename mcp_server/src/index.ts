a9e988205713b8bba33b9fbcb5a04ec1745132f6
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-25 17:53:44 +0530

    setup for mcp server

---
+++ mcp_server/src/index.ts
@@ -0,0 +1,9 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create server instance
+const server = new McpServer({
+  name: "tiny-cats",
+  version: "1.0.0",
+});
